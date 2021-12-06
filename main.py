cea4d29f665bc6cae46a864c64b7d3de794410e1
VadimBielinskii <>
2021-12-06 07:05:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def noteikDiapazonu(d1, d2, sk):
+  rezultats = "Skaitlis nav diapazona!"
+  if d1>=sk<=d2:
+    rezultats = "Skaitlis ir diapazona!"
+  return rezultats
+
+d1 = int(input("Ievadi diapazona sakumu: "))
+d2 = int(input("Ievadi diapazona beigas: "))
+sk = int(input("Ievadi skaitli: "))
+rez = noteikDiapazonu(d1,d2,sk)
+print(rez)
